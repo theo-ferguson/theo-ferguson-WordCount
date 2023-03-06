13307c33af1d2671c1f0d71252892c959bcedd39
theo ferguson <fergy546@yahoo.com>
2023-03-06 16:02:28 +0000

    complete

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -5,6 +5,7 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        String[] count = in.split(" ");
+        return count.length;
     }
 }
